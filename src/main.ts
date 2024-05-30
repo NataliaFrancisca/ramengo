ede1ec847d71dd22696289acdf3654ad0e95f27a
nataliaFrancisca <nathifrancisca@gmail.com>
2024-05-29 23:43:48 -0300

    feat: import the Header component

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import './sass/main.scss';
 import { setupCounter } from './counter.ts'
+import Header from './components/Header/Header.ts';
 
 document.querySelector<HTMLElement>('#page-home')!.innerHTML = `
- 
+    ${Header}
 `
 
 setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+
+
