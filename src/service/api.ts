55d50b69d19d18834745b6f1159d618a7ce1110c
nataliaFrancisca <nathifrancisca@gmail.com>
2024-06-02 21:22:55 -0300

    feat: created the function to fetch and post

---
+++ src/service/api.ts
@@ -0,0 +1,52 @@
+const API_KEY = import.meta.env.VITE_API_KEY;
+
+export async function fetchData(){
+
+    const [brothsResponse, proteinsResponse] = await Promise.all([
+        await fetch('https://api.tech.redventures.com.br/broths', {
+            headers: {
+                "x-api-key": API_KEY
+            }
+        }).then((data) => {
+
+            if(data){
+                return data.json()
+            }
+
+        }),
+    
+        await fetch('https://api.tech.redventures.com.br/proteins', {
+            headers: {
+                "x-api-key": API_KEY
+            }
+        }).then((data) => {
+            if(data){
+                return data.json();
+            }
+        })
+    ])
+
+
+    return { brothsResponse, proteinsResponse };
+}
+
+
+export async function postUserOrder(order: {brothId: string, proteinId: string}){
+
+    const response = await fetch('https://api.tech.redventures.com.br/order', {
+        method: "POST",
+        body: JSON.stringify(order),
+        headers: {
+            "x-api-key": API_KEY,
+            "Content-type": "application/json; charset=UTF-8"
+        },
+    })
+
+    if(response.ok){
+        return await response.json();
+    }
+
+    return 'something got wrong'
+}
+
+
