d08cf60c6db32a2474601fd74e6a14cb6f38da61
nataliafrancisca <nathifrancisca@gmail.com>
2024-06-05 23:36:12 -0300

    build: created file for vercel and vite config

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        success: path.resolve(__dirname, 'success.html')
+      }
+    }
+  }
+});
