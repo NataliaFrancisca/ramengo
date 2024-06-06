d4c763f0ef530053e97845cba3c8e52f70d5faf4
nataliafrancisca <nathifrancisca@gmail.com>
2024-06-05 22:25:36 -0300

    feat: created a folder for the illustration component

---
+++ src/components/Illustration/Illustration.ts
@@ -0,0 +1,9 @@
+const Illustration = `
+    <section id="group-illustration">
+        <img src="assets/images/entregadora.svg" id="illustration_entregadora" />
+        <img src="assets/images/balao-azul.svg" id="illustration_balao-azul" />
+        <img src="assets/images/balao-amarelo.svg" id="illustration_balao-amarelo"/>
+    </section>
+`;
+
+export default Illustration;
