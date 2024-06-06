d1818af559c68b74fa699015cd7d029b2a532848
nataliafrancisca <nathifrancisca@gmail.com>
2024-06-05 22:27:18 -0300

    feat: changed the localStorage for sessionStorage and check if the storage is empty

--- src/handler/handleSuccess.ts
+++ src/handler/handleSuccess.ts
@@ -1,13 +1,18 @@
 import { ISuccessResponse } from "../ts/interface";
 
-const localStorageResponse = localStorage.getItem('RAMENGO_ORDER');
-
+const sessionStorageResponse = sessionStorage.getItem('RAMENGO_ORDER');
 
 document.addEventListener("DOMContentLoaded", () => {
+
+    if(!sessionStorageResponse){
+        alert('Hey! You have to request your order :)');
+        window.location.href = "index.html"
+    }
+
     const orderImage = document.getElementById('order-data');
 
-    if(localStorageResponse && orderImage){
-        const orderData = JSON.parse(localStorageResponse) as ISuccessResponse;
+    if(sessionStorageResponse && orderImage){
+        const orderData = JSON.parse(sessionStorageResponse) as ISuccessResponse;
 
         orderImage.innerHTML += `
             <img src="${orderData.image}"></img>
