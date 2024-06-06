d8c0b9a7efc89972409e2d2408b63c0f5498f872
nataliafrancisca <nathifrancisca@gmail.com>
2024-06-05 22:23:50 -0300

    feat: deleted components

--- src/components/Illustration.ts
+++
@@ -1,9 +0,0 @@
-const Illustration = `
-    <div id="group-illustration">
-        <img src="assets/images/entregadora.svg" id="illustration_entregadora" />
-        <img src="assets/images/balao-azul.svg" id="illustration_balao-azul" />
-        <img src="assets/images/balao-amarelo.svg" id="illustration_balao-amarelo"/>
-    </div>
-`;
-
-export default Illustration;
