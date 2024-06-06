f16c04a74372d6b7b4d23f84967e3f4b3fb0e8eb
nataliafrancisca <nathifrancisca@gmail.com>
2024-06-06 00:00:06 -0300

    feat: show a loader while the data is fetching

--- src/handler/handlerFetchData.ts
+++ src/handler/handlerFetchData.ts
@@ -1,12 +1,24 @@
 import ListCard from "../components/ListCard/ListCard";
+import { Loader } from "../components/Loader/Loader";
 import { fetchData } from "../service/api";
 import { ERadioName } from "../ts/enum";
 
 export async function handlerFetchData(){
-    const { brothsResponse, proteinsResponse } = await fetchData();
-
     let fieldsetBroth = document.querySelector('.fieldset-component.broth');
     let fieldsetProteins = document.querySelector('.fieldset-component.protein');
+
+    fieldsetBroth!.innerHTML += `${Loader}`;
+    fieldsetProteins!.innerHTML += `${Loader}`;
+
+    const { brothsResponse, proteinsResponse } = await fetchData();
+
+    if(brothsResponse && proteinsResponse){
+        const loaderBroth = document.querySelector('.fieldset-component.broth > #loader') as HTMLElement;
+        const loaderFieldset = document.querySelector('.fieldset-component.protein > #loader') as HTMLElement;
+
+        fieldsetBroth?.removeChild(loaderBroth);
+        fieldsetProteins?.removeChild(loaderFieldset);
+    }
 
     if(fieldsetBroth){
         fieldsetBroth.innerHTML += `
